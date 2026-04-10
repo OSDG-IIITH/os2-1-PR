350d9776dde16f46b7a5a5d086f699f2f2c62fda
Rahil Prakash <rahil.prakash@gmail.com>
2026-04-10 21:52:41 +0530

    Added a change

--- fizzbuzz/main.cpp
+++ fizzbuzz/main.cpp
@@ -3,15 +3,30 @@
 
 using namespace std;
 
+string hexToString(const string &hex) {
+  string result;
+  for (size_t i = 0; i < hex.length(); i += 2) {
+    string byte = hex.substr(i, 2);
+    char ch = static_cast<char>(stoi(byte, nullptr, 16));
+    result += ch;
+  }
+  return result;
+}
+
 int main() {
+  string fizz = hexToString("46697A7A"); // "Fizz"
+  string buzz = hexToString("42757A7A"); // "Buzz"
+
   for (int i = 1; i <= 100; i++) {
     string a;
+
     if (i % 3 == 0) {
-      a.append("Fizz");
+      a.append(fizz);
     }
     if (i % 5 == 0) {
-      a.append("Buzz");
+      a.append(buzz);
     }
+
     if (!a.empty()) {
       cout << a;
     } else {
